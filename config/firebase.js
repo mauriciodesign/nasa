f6611e1d526b4bfcbc66e0131000d002533a874e
mauriciodesign <mauricio.a.design@gmail.com>
2020-08-05 18:38:21 -0400

    Delate folder

--- config/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyC4VPXsze7yz8cCPM2OW_QPyPhe7UXuS3o",
-    authDomain: "nasa-8835a.firebaseapp.com",
-    databaseURL: "https://nasa-8835a.firebaseio.com",
-    projectId: "nasa-8835a",
-    storageBucket: "nasa-8835a.appspot.com",
-    messagingSenderId: "407107466092",
-    appId: "1:407107466092:web:bb935f811078776ba27949"
-}
